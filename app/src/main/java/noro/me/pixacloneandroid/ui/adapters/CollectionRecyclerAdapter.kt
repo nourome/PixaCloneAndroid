46ac8bfd75ee7b49656d321f3b12d4441391d32c
nour <nour.saffaf@gmail.com>
2018-10-08 22:39:05 +0300

    removed images

--- app/src/main/java/noro/me/pixacloneandroid/ui/adapters/CollectionRecyclerAdapter.kt
+++ app/src/main/java/noro/me/pixacloneandroid/ui/adapters/CollectionRecyclerAdapter.kt
@@ -26,32 +26,6 @@ class CollectionRecyclerAdapter(val photos: ArrayList<PixaPhotoModel>, val dp: F
 
     lateinit var fragment: Fragment
 
-    companion object {
-        private val CAT_IMAGE_IDS = intArrayOf(
-                R.drawable.cat_1,
-                R.drawable.cat_2,
-                R.drawable.cat_3,
-                R.drawable.cat_4,
-                R.drawable.cat_5,
-                R.drawable.cat_6,
-                R.drawable.cat_7,
-                R.drawable.cat_8,
-                R.drawable.cat_9,
-                R.drawable.cat_10,
-                R.drawable.cat_11,
-                R.drawable.cat_12,
-                R.drawable.cat_13,
-                R.drawable.cat_14,
-                R.drawable.cat_15,
-                R.drawable.cat_16,
-                R.drawable.cat_17,
-                R.drawable.cat_18,
-                R.drawable.cat_19
-        )
-
-
-    }
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionRecyclerAdapter.CellHolder {
         val cellView = LayoutInflater.from(parent.context)
